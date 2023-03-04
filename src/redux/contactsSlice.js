26f4267624fe359f0fbe76189602afd746e003fe
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-03-04 21:24:38 +0200

    updated

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -4,12 +4,11 @@ import { nanoid } from '@reduxjs/toolkit';
 export const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
-    contactsList: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: {
+      items: [],
+      isLoading: false,
+      error: null,
+    },
     filter: '',
   },
   reducers: {
@@ -20,21 +19,21 @@ export const contactsSlice = createSlice({
     addContact: (state, action) => {
       console.log(action);
       const { name, number } = action.payload;
-      const isExist = state.contactsList.some(contact => {
+      const isExist = state.contacts.items.some(contact => {
         return contact.name === name;
       });
       if (isExist) {
         alert(`${name} is already in contacts.`);
         return;
       }
-      state.contactsList = [
-        ...state.contactsList,
+      state.contacts.items = [
+        ...state.contacts.items,
         { id: nanoid(), name, number },
       ];
     },
 
     deleteContact: (state, action) => {
-      state.contactsList = state.contactsList.filter(
+      state.contacts.items = state.contacts.items.filter(
         contact => contact.id !== action.payload
       );
     },
@@ -45,3 +44,5 @@ export const { onFilterChange, addContact, deleteContact } =
   contactsSlice.actions;
 
 export default contactsSlice.reducer;
+
+// https://64036bf1f61d96ac487f65de.mockapi.io/contacts
